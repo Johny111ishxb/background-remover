ff81ed54fee003a703a6fc0b0d242ff72f507938
Johny111ishxb <123812578+Johny111ishxb@users.noreply.github.com>
2024-09-30 19:50:27 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,17 @@
+from flask import Flask, request, send_file, render_template
+from flask_cors import CORS
+from rembg import remove
+from PIL import Image
+import io
+
+# Define the Flask app
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST"], allow_headers=["Content-Type"])
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Serve the HTML file
+
 @app.route('/upload', methods=['POST'])
 def upload_image():
     try:
@@ -7,19 +21,4 @@ def upload_image():
         image_file = request.files['image_file']
         input_image = Image.open(image_file.stream)
 
-        # Reduce image size to lower memory consumption
-        input_image.thumbnail((1024, 1024))  # Adjust the size as needed
-
-        # Process the image
-        output_image = remove(input_image)
-
-        # Save the output image to a byte stream
-        img_io = io.BytesIO()
-        output_image.save(img_io, 'PNG')
-        img_io.seek(0)
-
-        return send_file(img_io, mimetype='image/png')
-
-    except Exception as e:
-        print(f"Error processing the image: {str(e)}")
-        return f"Error processing the image: {str(e)}", 500
+        # Reduce image size 
