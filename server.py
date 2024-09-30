df3d2daade2889c7983a84b018a0dc4f095a6f3d
Johny111ishxb <123812578+Johny111ishxb@users.noreply.github.com>
2024-09-30 10:42:43 +0500

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS  # Import CORS
 from rembg import remove
 from PIL import Image
 import io
+import os  # Import os module for environment variable
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -23,4 +24,4 @@ def upload_image():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))  # Use PORT env variable
