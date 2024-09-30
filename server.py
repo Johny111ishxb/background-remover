fa7d1d30c732bb6658139e169f10fcd1a0f1f227
Johny111ishxb <123812578+Johny111ishxb@users.noreply.github.com>
2024-09-30 11:41:58 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, render_template  # Import render_template
+from flask import Flask, request, send_file, render_template
 from flask_cors import CORS
 from rembg import remove
 from PIL import Image
@@ -17,18 +17,14 @@ def upload_image():
         return 'No file uploaded', 400
 
     image_file = request.files['image_file']
-    
-    try:
-        input_image = Image.open(image_file.stream)
-        output_image = remove(input_image)
+    input_image = Image.open(image_file.stream).convert("RGBA")  # Ensure image is in RGBA mode
+    output_image = remove(input_image)
 
-        img_io = io.BytesIO()
-        output_image.save(img_io, 'PNG')
-        img_io.seek(0)
+    img_io = io.BytesIO()
+    output_image.save(img_io, 'PNG')
+    img_io.seek(0)
 
-        return send_file(img_io, mimetype='image/png')
-    except Exception as e:
-        return f'Error processing the image: {str(e)}', 500
+    return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)  # Listen on all addresses
+    app.run(host='0.0.0.0', port=8000, debug=True)  # Update host and port for deployment
