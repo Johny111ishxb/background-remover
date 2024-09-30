c86e9c9b6a2bfd4924ddea27ffa3eb4d04a7576e
Johny111ishxb <123812578+Johny111ishxb@users.noreply.github.com>
2024-09-30 14:24:31 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,37 +1,31 @@
-import os
-import logging
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template  # Import render_template
 from flask_cors import CORS
 from rembg import remove
 from PIL import Image
 import io
+import os  # Import os module
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable CORS for all routes
 
-logging.basicConfig(level=logging.DEBUG)  # Add this line to enable logging
+@app.route('/')
+def home():
+    return render_template('index.html')  # Serve the HTML file
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    logging.debug('Upload route triggered')
     if 'image_file' not in request.files:
-        logging.error('No file uploaded')
         return 'No file uploaded', 400
 
-    try:
-        image_file = request.files['image_file']
-        input_image = Image.open(image_file.stream)
-        logging.debug('Image uploaded successfully')
-        output_image = remove(input_image)
+    image_file = request.files['image_file']
+    input_image = Image.open(image_file.stream)
+    output_image = remove(input_image)
 
-        img_io = io.BytesIO()
-        output_image.save(img_io, 'PNG')
-        img_io.seek(0)
-        return send_file(img_io, mimetype='image/png')
-    except Exception as e:
-        logging.error(f'Error processing image: {e}')
-        return 'Internal server error', 500
+    img_io = io.BytesIO()
+    output_image.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)  # Update for deployment
