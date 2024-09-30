5801e41e8334b9c1bcc2dcfba45b5346aea260d7
Johny111ishxb <123812578+Johny111ishxb@users.noreply.github.com>
2024-09-30 14:04:04 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,11 @@
-from flask import Flask, request, send_file, render_template
-from flask_cors import CORS
+from flask import Flask, request, send_file
+from flask_cors import CORS  # Import CORS
 from rembg import remove
 from PIL import Image
 import io
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
-@app.route('/')
-def home():
-    return render_template('index.html')  # Serve the HTML file
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -27,5 +23,5 @@ def upload_image():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
-    # Bind to 0.0.0.0 and use the appropriate port for production
-    app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
+    
